f3139f6ce6e36ca1de657590d2352338ba71dae4
sinforxyz <3070089388@qq.com>
2026-02-06 11:36:57 +0800

    作业1-1

--- assignment_0/src/a0_01_temperature.cpp
+++ assignment_0/src/a0_01_temperature.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <cstdio> 
 #include "rm_a0/a0_01_temperature.hpp"
 
 int main() {
@@ -9,4 +9,17 @@ int main() {
   }
   std::cout << rm_a0::FormatFahrenheit(rm_a0::CelsiusToFahrenheit(celsius));
   return 0;
+}
+namespace rm_a0 {
+
+double CelsiusToFahrenheit(double celsius){
+  return celsius*9.0/5.0+32.0;
+}
+
+std::string FormatFahrenheit(double fahrenheit){
+  char buffer[64];
+  snprintf(buffer, sizeof(buffer), "%.2f\n", fahrenheit);
+  return std::string(buffer);
+}
+
 }
